07f5cd335fbcdcec39b9c2f2d6e2dc1416ff89f8
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 21:58:08 -0400

    Add Ripples/RipplesTests/RipplesTests.swift

---
+++ Ripples/RipplesTests/RipplesTests.swift
@@ -0,0 +1,17 @@
+//
+//  RipplesTests.swift
+//  RipplesTests
+//
+//  Created by Elliot Williams on 2025-07-13.
+//
+
+import Testing
+@testable import Ripples
+
+struct RipplesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
